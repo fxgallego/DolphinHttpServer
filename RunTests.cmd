70fa0611f2cf71bb11433a6b4af27028e14be29c
Sebastian Calvo <fxgallego@gmail.com>
2019-05-04 15:18:07 -0300

    Update RunTests.cmd

--- RunTests.cmd
+++ RunTests.cmd
@@ -1,9 +1,7 @@
-dir /ah C:\projects\Dolphin
 @ECHO OFF
-SET PATH=C:\projects\Dolphin;%PATH%
 ECHO Running regression tests
 echo. >DPRO.errors
-Dolphin7 ..\DPRO.img7 -u -f RegressionTestsRun.st -q
+Dolphin7 -u -f RegressionTestsRun.st -q
 set errorCode=%ERRORLEVEL%
 TYPE DPRO.errors
 if NOT "%APPVEYOR%"=="" powershell.exe -file UploadTestResults.ps1

4e6071fb2204cf12f120ff2344488395671f5977
Sebastian Calvo <scalvo@infoil.com.ar>
2019-05-04 11:05:22 -0300

    Added appveyor config and test suit run scripts.

---
+++ RunTests.cmd
@@ -0,0 +1,8 @@
+@ECHO OFF
+ECHO Running regression tests
+echo. >DPRO.errors
+Dolphin7 ..\DPRO.img7 -u -f RegressionTestsRun.st -q
+set errorCode=%ERRORLEVEL%
+TYPE DPRO.errors
+if NOT "%APPVEYOR%"=="" powershell.exe -file UploadTestResults.ps1
+EXIT /b %errorCode%
